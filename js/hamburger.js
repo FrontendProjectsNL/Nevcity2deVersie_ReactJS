d426f7e401810e5047013d2e400389aa83b3b1b9
Ehsan Abadi <[e.abadi@noviuniversity.com]>
2022-01-08 00:48:54 +0100

    hamburger-menu ready

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,15 @@
+// const checkbox = document.getElementById('hamburger-checkbox');
+// const link1 = document.getElementById('link1');
+// link1.addEventListener('click', () => {
+//     checkbox.click();
+// });
+
 const checkbox = document.getElementById('hamburger-checkbox');
-const link1 = document.getElementById('link1');
-link1.addEventListener('click', () => {
-    checkbox.click();
-});
+
+const navLinks = document.querySelectorAll(".link-item");
+	[...navLinks].forEach(navLink => {
+			navLink.addEventListener('click', () => {
+     checkbox.click();
+	});
+})
+
