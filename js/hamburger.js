c925031bd008bcedb1b8c569c54ff44bc423b4d6
Ehsan Abadi <[e.abadi@noviuniversity.com]>
2022-01-11 15:42:57 +0100

    animaties toegevoegd

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,4 @@
-// const checkbox = document.getElementById('hamburger-checkbox');
-// const link1 = document.getElementById('link1');
-// link1.addEventListener('click', () => {
-//     checkbox.click();
-// });
+
 
 const checkbox = document.getElementById('hamburger-checkbox');
 
